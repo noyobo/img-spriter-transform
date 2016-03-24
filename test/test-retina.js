28abba5b3e2e2e3004f9809c71b87954f9efb3bb
noyobo <nongyoubao@alibaba-inc.com>
2016-03-24 23:18:16 +0800

    test: travis

--- test/test-retina.js
+++ test/test-retina.js
@@ -3,12 +3,17 @@ var transfrom = require('../index.js');
 var data = require('./data-retina.js');
 var fs = require('fs');
 var path = require('path');
+var assert = require('assert');
+
+var expectedCss =
+  fs.readFileSync(path.join(__dirname, './sprite-retina.css'))
+  .toString();
 
 transfrom(data)
-  .then(function(css) {
-    console.log(css);
-    fs.writeFile(path.join(__dirname, './sprite-retina.css'), css);
+  .then(css => {
+    assert.equal(css, expectedCss, 'good job');
   })
   .catch(err => {
     console.log(err);
-  })
+    process.exit(1);
+  });
--- test/test.js
+++ test/test.js
@@ -3,12 +3,17 @@ var transfrom = require('../index.js');
 var data = require('./data.js');
 var fs = require('fs');
 var path = require('path');
+var assert = require('assert');
 
+var expectedCss =
+  fs.readFileSync(path.join(__dirname, './sprite.css'))
+  .toString();
 
 transfrom(data)
-  .then(function(css) {
-    console.log(css);
-    fs.writeFile(path.join(__dirname, './sprite.css'), css);
-  }).catch(err => {
-    console.log(err);
+  .then(css => {
+    assert.equal(css, expectedCss, 'good job');
   })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
